61356e7d1eaf7639f157d0cd42fdb3fef1ab373f
Cang Le <cang.b.le@gmail.com>
2017-10-21 20:51:15 -0700

    something

--- testing-example/.storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@storybook/react';
-
-function loadStories() {
-  require('../src/index.css');
-  require('../src/stories/MenuItemComponent.story');
-  require('../src/stories/MenuComponent.story');
-}
-
-configure(loadStories, module);
--- testing-example/src/Foo.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Foo from './Foo';
-
-describe('a demo', () => {
-  it('should render', () => {
-    expect(shallow(<Foo />).is('.foo')).toBe(true)
-  })
-})
